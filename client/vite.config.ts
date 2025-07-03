dd71d7ea396f49bd15411c9f75ee9166f438fe96
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2025-07-04 00:14:00 +0200

    Vite sourcemaps fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs'
 
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), fixSourceMaps()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -23,3 +25,36 @@ export default defineConfig({
     }
   }
 })
+
+function fixSourceMaps(): Plugin {
+  let currentInterval: any = null
+  return {
+    name: 'fix-source-map',
+    enforce: 'post',
+    transform: function (source: any) {
+      if (currentInterval) {
+        return
+      }
+      currentInterval = setInterval(function () {
+        const nodeModulesPath = path.join(__dirname, 'node_modules', '.vite', 'deps')
+        if (fs.existsSync(nodeModulesPath)) {
+          clearInterval(currentInterval)
+          currentInterval = null
+          const files = fs.readdirSync(nodeModulesPath)
+          files.forEach(function (file) {
+            const mapFile = file + '.map'
+            const mapPath = path.join(nodeModulesPath, mapFile)
+            if (fs.existsSync(mapPath)) {
+              const mapData = JSON.parse(fs.readFileSync(mapPath, 'utf8'))
+              if (!mapData.sources || mapData.sources.length == 0) {
+                mapData.sources = [path.relative(mapPath, path.join(nodeModulesPath, file))]
+                fs.writeFileSync(mapPath, JSON.stringify(mapData), 'utf8')
+              }
+            }
+          })
+        }
+      }, 100)
+      return source
+    }
+  }
+}
