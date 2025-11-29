427ea751430a655633b2da15c01a3f12c7666097
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2025-11-29 16:04:06 +0100

    Change kotlin version to fix coroutine error with bggclient

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.audux.bgg:bggclient:1.3.0") {
         exclude(group = "org.slf4j", module = "slf4j-simple")
     }
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
